6907b3e8ffcdb01b50744f59d9eb1f7947514c6b
BBGaster <124209517+BBGaster@users.noreply.github.com>
2024-11-11 15:53:42 +0100

    creazione product

---
+++ src/main/java/com/apuliadigitalmaker/buytopia/product/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.apuliadigitalmaker.buytopia.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    @Query("SELECT e FROM Product e WHERE e.deletedAt IS NULL")
+    List<Product> findNotDeleted();
+    //findAllDeletedIsNull
+
+    @Query("SELECT e FROM Product e WHERE e.id = :id AND e.deletedAt IS NULL")
+    Optional<Product> findByIdNotDeleted(@Param("id") Long id);
+    //findByIdDeletedIsNull(Long id)
+
+    List<Product> findByNameStartsWithIgnoreCaseAndDeletedAtIsNull(String name);
+}
